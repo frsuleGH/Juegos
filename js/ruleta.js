ee3fc242060b61adbb0d00b7e73197ab7340bb32
FranciscoSule <franciscosule@gmail.com>
2023-12-22 11:39:18 -0300

    Update ruleta.js

--- js/ruleta.js
+++ js/ruleta.js
@@ -10,7 +10,7 @@ function ruleta() {
     let contenido = document.querySelector('#contenido');
 
     setTimeout(function() {
-        if (aleatorio === 6|| aleatorio ===9) {
+        if (aleatorio === 6|| aleatorio === 9 || aleatorio === 1) {
             let elemento = document.createElement('h2');
             elemento.textContent = 'SHOT🍺!';
             elemento.classList.add('rojo');
